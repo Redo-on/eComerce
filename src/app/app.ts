350e870c5e073a3596f7401b04512a509c854727
Redon <redonkurtaj2006@gmail.com>
2025-06-17 15:10:04 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'BankLoan';
+    stepsList: any [] = [
+      {
+        stepName: 'Basic Details',
+        isCompltete: false
+      },
+      {
+        stepName: 'Skills',
+        isCompltete: false
+      },
+      {
+        stepName: 'Experience',
+        isCompltete: false
+      }
+    ]
+    activeStep:any = this.stepsList[0];
+
+    setCurrentStep(activeStep: any) {
+      this.activeStep = activeStep;
+    }
+    
 }
